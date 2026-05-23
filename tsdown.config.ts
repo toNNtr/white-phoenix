0e77b90e51dbd52df1121cdbe529251d4319fddf
toNNtr <savkov.andrei@mail.ru>
2026-05-23 19:56:09 +0300

    playground заменен на vitepress tsconfig разделен на части Удален лишний tsconfig.tsbuildinfo - tsbuildinfo файлы перемещены в ./node_modules/.tmp/...

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "tsdown";
 import svg from "@tonntr/rolldown-plugin-svg-loader";
+import vue from "unplugin-vue/rolldown";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-    plugins: [svg()],
+    plugins: [vue(), svg()],
     entry: {
         main: "src/main.ts",
         shared: "src/shared/index.ts",
@@ -11,5 +13,8 @@ export default defineConfig({
     platform: "neutral",
     exports: true,
     fromVite: true,
-    dts: { vue: true },
+    dts: { vue: true, tsconfig: "./tsconfig.lib.json" },
+    alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
 });
