1432eae6bf58fcd28f9e156b0176d2ff12f49eb4
toNNtr <savkov.andrei@mail.ru>
2026-06-06 22:18:04 +0300

    Включены чистые URL

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
     title: "White Phoenix Docs",
     description: "White Phoenix Docs",
+    cleanUrls: true,
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
