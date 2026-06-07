cb02741cab70248869b096e7c2daaeb227a111ee
toNNtr <savkov.andrei@mail.ru>
2026-06-07 23:32:55 +0300

    Удален layout

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,9 @@ import * as buttonGroup from "@/ui/button-group";
 import * as card from "@/ui/card";
 import * as dropdown from "@/ui/dropdown";
 import * as icon from "@/ui/icon";
-import * as layout from "@/ui/layout";
 import * as modal from "@/ui/modal";
 import * as catalog from "@/ui/catalog";
 
 export type { common, utility };
 export const api = { helpers, popup };
-export const ui = { button, buttonGroup, card, dropdown, icon, layout, modal, catalog };
+export const ui = { button, buttonGroup, card, dropdown, icon, modal, catalog };
