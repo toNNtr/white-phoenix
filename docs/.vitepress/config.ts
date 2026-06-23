0227794450e1e2d3d5e9b728ee0b3afb17c47371
toNNtr <savkov.andrei@mail.ru>
2026-06-24 00:54:41 +0300

    Описан компонент BaseCatalog

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
                 {
                     text: "Представление",
                     items: [
+                        { text: "BaseCatalog", link: "/reference/ui/base-catalog" },
                         { text: "CardMedium", link: "/reference/ui/card-medium" },
                         { text: "CardSmall", link: "/reference/ui/card-small" },
                     ],
