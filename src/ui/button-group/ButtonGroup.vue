984e74bc5750f450da12385144030b02060368e4
toNNtr <savkov.andrei@mail.ru>
2026-06-30 23:05:35 +0300

    Исправлено отображение при использовании раскладки vertical

--- src/ui/button-group/ButtonGroup.vue
+++ src/ui/button-group/ButtonGroup.vue
@@ -33,7 +33,15 @@ const buttonGroupClass = ref({
 }
 
 .button-group.button-group_vertical {
+    width: fit-content;
     flex-direction: column;
+}
+
+.button-group.button-group_vertical > :slotted(button),
+.button-group.button-group_vertical > :slotted(a),
+.button-group.button-group_vertical > :slotted(div),
+.button-group.button-group_vertical > :slotted(div) {
+    width: 100%;
 }
 
 .button-group > :slotted(button),
@@ -42,14 +50,29 @@ const buttonGroupClass = ref({
 .button-group > :slotted(div) > a {
     border-radius: 0;
 }
+
 .button-group.button-group_rounded > *:first-child,
 .button-group.button-group_rounded > :slotted(div):first-child > a {
-    border-top-left-radius: 12px;
-    border-bottom-left-radius: 12px;
+    border-radius: 12px 0 0 12px;
 }
+
 .button-group.button-group_rounded > *:last-child,
 .button-group.button-group_rounded > :slotted(div):last-child > a {
-    border-top-right-radius: 12px;
-    border-bottom-right-radius: 12px;
+    border-radius: 0 12px 12px 0;
+}
+
+.button-group.button-group_rounded > *:only-child,
+.button-group.button-group_rounded > :slotted(div):only-child > a {
+    border-radius: 12px;
+}
+
+.button-group.button-group_vertical.button-group_rounded > *:first-child,
+.button-group.button-group_vertical.button-group_rounded > :slotted(div):first-child > a {
+    border-radius: 12px 12px 0 0;
+}
+
+.button-group.button-group_vertical.button-group_rounded > *:last-child,
+.button-group.button-group_vertical.button-group_rounded > :slotted(div):last-child > a {
+    border-radius: 0 0 12px 12px;
 }
 </style>
