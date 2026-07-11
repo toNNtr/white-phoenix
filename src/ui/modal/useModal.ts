a80c3afc5193128439b4a545358dbb8fe74c3380
toNNtr <savkov.andrei@mail.ru>
2026-07-11 16:45:36 +0300

    Обновлена логика модальных окон

--- src/ui/modal/useModal.ts
+++ src/ui/modal/useModal.ts
@@ -1,26 +1,25 @@
-import { ECloseReason, usePopup, type Popup } from "@/api/popup";
-import { computed, shallowRef, type ComponentOptionsMixin, type DefineComponent } from "vue";
+import { ECloseReason, usePopup, type Popup, type PopupCloseExtraParams } from "@/api/popup";
+import { computed, type ComponentOptionsMixin, type DefineComponent } from "vue";
 
-export type Modal<T extends Record<string | number | symbol, unknown> = Record<string, unknown>> =
-    Popup<T> & {
-        // prettier-ignore
-        component: DefineComponent<
+export type Modal<T extends PopupCloseExtraParams = Record<string, unknown>> = Popup<T> & {
+    // prettier-ignore
+    component: DefineComponent<
             // eslint-disable-next-line
             {},{},{},{},{},
             ComponentOptionsMixin,
             ComponentOptionsMixin,
             { close: (params?: Partial<{ reason: ECloseReason } & T>) => unknown }
         >;
-    };
+};
 
-export const modalList: Modal<Record<string | number | symbol, unknown>>[] = [];
+export const modalList: Modal[] = [];
 
 export function useOpenedModals() {
-    return computed(() => modalList.filter((modal) => modal.isOpened.value));
+    return computed(() => modalList.filter((modal) => modal.isOpenedRef.value));
 }
 
 export function useHasOpenedModals() {
-    return computed(() => modalList.filter((modal) => modal.isOpened.value).length);
+    return computed(() => modalList.filter((modal) => modal.isOpenedRef.value).length);
 }
 
 export function closeTopModal(reason?: ECloseReason) {
@@ -31,17 +30,20 @@ export function closeTopModal(reason?: ECloseReason) {
     }
 }
 
-export function useModal<T extends Record<string | number | symbol, unknown>>(
-    component: Modal<T>["component"],
-    type?: string,
-) {
+export function useModal<T extends Modal>(component: T["component"], type?: string) {
     const modalPopup = usePopup<T>({ type });
     const modal = {
         ...modalPopup,
         component,
     };
 
-    // @ts-expect-error Пока не знаю как решить это место.
+    modal.onBeforeDestroyed(() => {
+        const modalIdx = modalList.findIndex((elem) => elem === modal);
+        if (modalIdx >= 0) {
+            modalList.splice(modalIdx, 1);
+        }
+    });
+
     modalList.push(modal);
 
     return modalPopup;
