2af08b66a5a7b1bf34980d018bdd8ed914c48ce8
toNNtr <savkov.andrei@mail.ru>
2026-07-12 21:02:22 +0300

    Установлен плагин @vitejs/plugin-vue для тестирование компонентов Vue

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vitest/config";
 import { playwright } from "@vitest/browser-playwright";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
+    plugins: [vue()],
     test: {
         browser: {
             provider: playwright(),
