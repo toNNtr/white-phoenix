04fc7deac3122864857b38622068eeba0f54509f
toNNtr <savkov.andrei@mail.ru>
2026-07-19 00:17:13 +0300

    Добавлен алиас для @

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vitest/config";
 import { playwright } from "@vitest/browser-playwright";
 import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "node:url";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
     plugins: [vue()],
     test: {
         browser: {
