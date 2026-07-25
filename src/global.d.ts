033cd6a571f72d9b1e51b16b03a691b1a1236fff
toNNtr <savkov.andrei@mail.ru>
2026-07-25 23:25:11 +0300

    Типы для импорта css

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,10 @@ declare module "*?raw" {
     const src: string;
     export default src;
 }
+
+declare module "*.css";
+
+declare module "*.module.css" {
+    const classes: { readonly [key: string]: string };
+    export default classes;
+}
